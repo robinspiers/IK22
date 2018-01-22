32fbfd335842de9d126729cc1c8db229b3b74d1d
Justin Mensah <justin.mensah97@gmail.com>
2018-01-22 11:48:58 +0000

    app.route pregame aangepast

--- triviaroyale/application.py
+++ triviaroyale/application.py
@@ -5,7 +5,6 @@ from passlib.apps import custom_app_context as pwd_context
 from tempfile import mkdtemp
 
 from helpers import *
-from models import *
 
 # configure application
 app = Flask(__name__)
@@ -51,14 +50,15 @@ def login():
             return apology("Must provide password")
 
         # query database for username
-        username = find_user(request.form.get("username"), request.form.get("password"))
+        rows = db.execute("SELECT * FROM users WHERE username = :username", \
+                          username = request.form.get("username"))
 
         # ensure username exists and password is correct
-        #if username == False:
-         #   return apology("Invalid username and/or password")
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+            return apology("Invalid username and/or password")
 
         # remember which user has logged in
-        session["user_id"] = keepLoggedIn(username)
+        session["user_id"] = rows[0]["id"]
 
         # redirect user to home page
         return redirect(url_for("index"))
@@ -97,11 +97,13 @@ def register():
             return apology("Submitted passwords are not identical")
 
         # insert new user into users, store hash of the password
-        new_user = insert_user(request.form.get("username"), request.form.get("password"))
+        new_user = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)", \
+                             username = request.form.get("username"), \
+                             hash = pwd_context.hash(request.form.get("password")))
 
         # ensure username does not already exist
-        #if new_user == False:
-         #   return apology("Username already exists")
+        if not new_user:
+            return apology("Username already exists")
 
         # keep user logged in
         session["user_id"] = new_user
@@ -113,6 +115,12 @@ def register():
     else:
         return render_template("register.html")
 
-#@app.route("/pregame")
-#def index():
-    #return render_template("pregame.html")
+@app.route("/pregame", methods = [])
+def pregame():
+
+    if request.method == "POST":
+
+        # Als gebruikers op pregame komen via GET
+        return redirect(url_for("question"))
+
+    return render_template("pregame.html")
