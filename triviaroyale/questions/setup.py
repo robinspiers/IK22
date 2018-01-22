cb4871051023d9ce27bd52b98842626e2f8d1b3a
Timo Gerardus Pieter Rossenaar <timo.rossenaar@gmail.com>
2018-01-22 12:07:17 +0000

    questions file aangemaakt

---
+++ triviaroyale/questions/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+  name = 'opentdb',
+  packages = ['opentdb'],
+  version = '1.01',
+  description = 'A library for interacting with the Open Trivia Database (openTDB) API.'
+)
