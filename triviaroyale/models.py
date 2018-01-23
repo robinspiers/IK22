5303162403214e5fc96db61585be566c6b0c178e
Justin Mensah <justin.mensah97@gmail.com>
2018-01-23 23:55:24 +0000

    Updated User model class with Flask-Login methods

--- triviaroyale/models.py
+++ triviaroyale/models.py
@@ -10,10 +10,24 @@ class User(db.Model):
 
     __tablename__ = "users"
     id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column('username', db.Text)
+    username = db.Column('username', db.Text, unique=True, index=True)
     password = db.Column('password', db.Text)
 
     def __init__(self, username, password):
         self.username = username
         self.password = pwd_context.hash(password)
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
