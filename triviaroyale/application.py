b0b59f8554a46a7d73cb5ab12d62c9fea4c17a7d
Timo Gerardus Pieter Rossenaar <timo.rossenaar@gmail.com>
2018-01-24 10:26:27 +0000

    test

--- triviaroyale/application.py
+++ triviaroyale/application.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 from flask_login import login_user , logout_user , current_user , login_required
 
 
-from helpers import *
+import helpers
 
 # configure application
 app = Flask(__name__)
@@ -31,6 +31,8 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 from models import User
+from questions import *
+
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
@@ -125,9 +127,18 @@ def pregame():
 
     # "POST" method
     if request.method == "POST":
-        return redirect(url_for("question"))
+        return render_template("pregame.html")
 
     # "GET" method
     else:
-        return render_template("pregame.html")
+        # get two random categories from the dictionary
+        category1 = randomcategory()
+        category2 = randomcategory()
 
+        # get a question with the demanded category from the api
+        if request.form.get("category1"):
+            Question.category = categories[category1]
+        if request.form.get("category2"):
+            Question.category = categories[category2]
+
+        return redirect(url_from("question"))
