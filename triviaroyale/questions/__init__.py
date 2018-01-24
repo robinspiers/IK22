b0331bc7cc498aa9eff2395406d6e295aa84a456
Timo Gerardus Pieter Rossenaar <timo.rossenaar@gmail.com>
2018-01-24 09:45:15 +0000

    begin pregame en question.html

--- triviaroyale/questions/__init__.py
+++
@@ -1,4 +0,0 @@
-#from api import Client
-#from exceptions import (OpenTDBException, HTTPError, ConnectionError, APIError)
-#from categories import categories
-#from question import Question
--- triviaroyale/questions/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(
-  name = 'opentdb',
-  packages = ['opentdb'],
-  version = '1.01',
-  description = 'A library for interacting with the Open Trivia Database (openTDB) API.'
-)
