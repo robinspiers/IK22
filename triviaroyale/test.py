68fb88a74368699f2ad4237da26e02ceeda588b1
Robin Spiers <31687221+robinspiers@users.noreply.github.com>
2018-02-01 09:07:32 +0100

    SORRY!! vergeten oefen bestand te verwijderen!!!

--- triviaroyale/test.py
+++
@@ -1,9 +0,0 @@
-answerdict = {}
-if Results.query.get(1).correct_answer == Results.query.get(1).answer1:
-    answerdict.append("answer1" : Results.query.get(1).correct_answer)
-elif Results.query.get(1).correct_answer == Results.query.get(1).answer2:
-    answerdict.append("answer2" : Results.query.get(1).correct_answer)
-elif Results.query.get(1).correct_answer == Results.query.get(1).answer3:
-    answerdict.append("answer3" : Results.query.get(1).correct_answer)
-elif Results.query.get(1).correct_answer == Results.query.get(1).answer4:
-    answerdict.append("answer4" : Results.query.get(1).correct_answer)
